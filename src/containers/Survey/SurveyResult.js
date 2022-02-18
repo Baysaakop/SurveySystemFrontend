1c3032743a34ed65e4e3027a469bf5082f44b594
Baysaakop <baysaakop@gmail.com>
2022-02-18 16:53:33 +0800

    4th commit

--- src/containers/Survey/SurveyResult.js
+++ src/containers/Survey/SurveyResult.js
@@ -1,8 +1,8 @@
+import { Col, Progress, Row, Spin, Typography } from "antd";
 import { useEffect, useState } from "react";
 import SurveyService from "./SurveyService";
 
 function SurveyResult (props) {
-    const [data, setData] = useState()
     const [survey, setSurvey] = useState()
 
     useEffect(() => {        
@@ -10,23 +10,76 @@ function SurveyResult (props) {
     }, [])
 
     function getData() {
-        const survey_id = props.match.params.survey_id
-        setSurvey(survey_id)
+        const survey_id = props.match.params.survey_id        
         SurveyService
-        .getUserResponses(survey_id)
+        .getSurveyById(survey_id)
         .then(res => {
             console.log(res.data)
-            setData(res.data)
+            setSurvey(res.data)
         })
         .catch(err => {
             console.log(err)
         })
     }
 
-    return (
-        <div>
+    function getTotalAnswer(answer_id, question_id) {
+        let count = 0
+        survey.userResponses.forEach(userResponse => {
+            userResponse.responses.forEach(response => {
+                if (response.question.id === question_id) {
+                    if (response.answer.id === answer_id) {
+                        count++
+                    }
+                }
+            })
+        });
+        return count
+    }
 
-        </div>
+    function getPercentage(answer_id, question_id) {
+        let answers_count = getTotalAnswer(answer_id, question_id)
+        let percent = (answers_count / survey.userResponses.length) * 100
+        console.log(percent)
+        return percent        
+    }
+
+    return (
+        survey ? (
+            <div>
+                <Typography.Title level={4}>
+                    Result of survey: {survey.survey}
+                </Typography.Title>
+                <Typography.Text>
+                    {survey.description}
+                </Typography.Text>
+                <Row gutter={16}>                                        
+                    {survey.questions.map(question => (
+                        <Col span={8} style={{ marginTop: '16px' }}>
+                            <div style={{ background: '#fff', padding: '16px' }}>
+                                <Typography.Title level={5}>
+                                    {question.question}
+                                </Typography.Title>
+                                {question.answers.map(answer => (
+                                    <div>                                        
+                                        <Typography.Text>
+                                            {answer.answer}
+                                        </Typography.Text>
+                                        <Progress 
+                                            percent={getPercentage(answer.id, question.id)} 
+                                            format={() => getTotalAnswer(answer.id, question.id)} 
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        ) : (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                <Spin />
+            </div>
+        )        
     )
 }
 
